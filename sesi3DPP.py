810d24b554846a9eb8b870d4cf4b821e7a828060
Nadila JM <96558056+nadilajm@users.noreply.github.com>
2023-01-31 00:26:53 +0700

    Add files via upload

---
+++ sesi3DPP.py
@@ -0,0 +1,36 @@
+#no1
+p = """ jenis-jenis tipe data:
+ a. integer
+ adalah sebuah tipe data yang berisikan bilangan bulat seperti 1,2,3,...
+ b. float
+ adalah sebuah tipe data yang berisikan bilangan real atau pecahan seperti 2,4 , 2,3 ,...
+ c.string
+ adalah sebuah tipe data yang berisikan karakter atau teks seperti a,b,nada,melodi
+ d. complex
+ adalah sebuah tipe data yang berisikan tipe data yang menampung bilangan komplek atau sering juga disebut bilangan imajiner. """
+print(p)
+
+# no 2
+# Nadila Jannatul Ma'wa
+# TI22C
+# 20220040199
+
+# no 3
+angka1 = 5000
+angka2 = 2000
+angka3 = 200
+a = 500 
+b = angka1 + a
+c = angka2 + a
+print(b,c)
+print(b + angka2 - angka3)
+
+# no4
+# rumus keliling lingkaran 2 x phi x r
+a = int(input('masukan nilai jari-jari:'))
+b = 2 * 3.14 * a
+print(b)
+# rumus volume kubus s^3
+c = int(input('masukan nilai sisi:'))
+d = c**3
+print(d)
